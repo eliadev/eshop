3464bb668a2bf5ac347e79de1fe54127c55e6f85
Elie Andraos <elie@andraos.org>
2020-06-16 11:23:48 +0300

    skus and quantity count for products

--- app/Product.php
+++ app/Product.php
@@ -9,6 +9,10 @@ class Product extends Model
 	protected $table = "products";
 	protected $fillable = ['name', 'description', 'price', 'reference', 'featured', 'published', 'brand_id'];
     
+    protected $with = [ 'skus' ];
+
+    protected $appends = [ 'quantity' ];
+
     public function brand() {
         return $this->belongsTo('App\Brand');
     }
@@ -20,5 +24,9 @@ class Product extends Model
 	public function skus()
     {
         return $this->hasMany('App\Sku');
+    }
+
+    public function getQuantityAttribute() {
+        return $this->skus->count();
     }
 }
